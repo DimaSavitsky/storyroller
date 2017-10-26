95c8bbae832491c45c06c60ee2df41868d574899
Dmitry Savitski <dimasavitski@gmail.com>
2017-10-26 16:22:54 +0300

    Initial commit

---
+++ config/environment.rb
@@ -0,0 +1,49 @@
+require 'bundler/setup'
+require 'hanami/setup'
+require 'hanami/model'
+require_relative '../lib/storyroller'
+require_relative '../apps/web/application'
+
+Hanami.configure do
+  mount Web::Application, at: '/'
+
+  model do
+    ##
+    # Database adapter
+    #
+    # Available options:
+    #
+    #  * SQL adapter
+    #    adapter :sql, 'sqlite://db/storyroller_development.sqlite3'
+    #    adapter :sql, 'postgresql://localhost/storyroller_development'
+    #    adapter :sql, 'mysql://localhost/storyroller_development'
+    #
+    adapter :sql, ENV['DATABASE_URL']
+
+    ##
+    # Migrations
+    #
+    migrations 'db/migrations'
+    schema     'db/schema.sql'
+  end
+
+  mailer do
+    root 'lib/storyroller/mailers'
+
+    # See http://hanamirb.org/guides/mailers/delivery
+    delivery :test
+  end
+
+  environment :development do
+    # See: http://hanamirb.org/guides/projects/logging
+    logger level: :debug
+  end
+
+  environment :production do
+    logger level: :info, formatter: :json, filter: []
+
+    mailer do
+      delivery :smtp, address: ENV['SMTP_HOST'], port: ENV['SMTP_PORT']
+    end
+  end
+end
---
+++ lib/storyroller.rb
@@ -0,0 +1,2 @@
+module Storyroller
+end
